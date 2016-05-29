c1b24955e07998d22f17878fa145cf3965a114d2
Titus Woo <titus.woo@me.com>
2016-05-29 18:49:25 -0400

    dev improvements - using watcher and file cache

---
+++ dev.webpack.config.js
@@ -0,0 +1,45 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+module.exports = {
+  context: path.join(__dirname, 'app'),
+  entry: {
+    'content': './content/index.js',
+    'background': './background/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]/index.js'
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    root: './app',
+    alias: {
+      shared: path.resolve('./shared'),
+      content: path.resolve('./app/content')
+    },
+    extensions: ['', '.webpack.js', '.web.js', '.js', '.css']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-object-rest-spread']
+        },
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  }
+}
